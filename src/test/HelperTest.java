7013dcbfa8748582c1a7ecbbffe7093a0046c2c4
FelixBriz7 <felixbriz077@gmail.com>
2026-05-20 13:37:58 +0200

    HELPER_TEST Dentro de HelperTest se a realizado los siguientes pruebas para verificar su funcionamiento:
    - void calcularEdadPersonaNormal()
    
    - void calcularEdadFechaNula()
    
    - void calcularEdadDevuelvePositivo()
    
    Cada uno de estas pruebas de JUnit a dado un correcto funcioanmiento, an pasado las puebas

--- src/test/HelperTest.java
+++ src/test/HelperTest.java
@@ -1,10 +1,28 @@
+import Examen.Base_de_datos.Helper;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelperTest {
 
     @Test
-    void calcularEdad() {
+    void calcularEdadPersonaNormal() {
+
+        assertTrue(Helper.calcularEdad(LocalDate.of(1990, 1, 1)) >= 35, "Debería tener al menos 35 años");
+
+    }
+
+    @Test
+    void calcularEdadFechaNula() {
+        assertEquals(0, Helper.calcularEdad(null), "Con fecha nula debe devolver 0");
+    }
+
+
+    @Test
+    void calcularEdadDevuelvePositivo() {
+        assertTrue(Helper.calcularEdad(LocalDate.of(2000, 1, 1)) >= 0, "La edad nunca puede ser negativa");
     }
 }
+
